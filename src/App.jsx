f8d5db5fcbc38fa88a709e5527d4a37426fe68e4
sz-leandro-cunha <leandro.frangit@gmail.com>
2025-05-15 11:49:34 -0300

    150525 - evento

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from './components/CoreConcept.jsx';
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleClick() {
+    console.log("teste");
+  }
+
   return (
     <div>
       <Header />
@@ -10,15 +15,20 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}
-            />
-            <CoreConcept {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcept {...CORE_CONCEPTS[3]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={handleClick}>Components</TabButton>
+            <TabButton onClick={handleClick}>JSX</TabButton>
+            <TabButton onClick={handleClick}>Props</TabButton>
+            <TabButton onClick={handleClick}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>

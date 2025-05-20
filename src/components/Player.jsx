4ed5b208bf2fc4b9a4a92c404dc55768909bf001
sz-leandro-cunha <leandro.cunha@szsolucoes.com.br>
2025-05-20 14:03:20 -0300

    jeito antigo

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("unknown entity");
+  const [inputPlayerName, setInputPlayerName] = useState("");
+
+  function handleClick() {
+    setPlayerName(inputPlayerName);
+  }
+  function handleChange(event) {
+    setInputPlayerName(event.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={inputPlayerName} onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
